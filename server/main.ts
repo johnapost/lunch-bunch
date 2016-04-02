8e57bd2db6d732cc0248b43ce0aa0dd652f110ef
johnapost <notimetobeidle@gmail.com>
2016-04-01 22:39:46 -0400

    Create sample api endpoint for yelp using express route

--- server/main.ts
+++ server/main.ts
@@ -4,12 +4,18 @@
 import express = require('express')
 import http = require('http')
 import bodyParser = require('body-parser')
-// import auth = require('./auth')
+var yelpApi = require('yelp')
+var yelp = new yelpApi({
+  consumer_key: process.env.LUNCH_CON_KEY,
+  consumer_secret: process.env.LUNCH_CONS_SEC,
+  token: process.env.LUNCH_TOK,
+  token_secret: process.env.LUNCH_TOK_SEC
+})
 
-// Flip this back and forth to oallow
+// Flip this back and forth to allow CORS
 var allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4000')
-  // res.header('Access-Control-Allow-Origin', '*')
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:4000')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
   res.header('Access-Control-Allow-Headers', ['Content-Type', 'X-Auth'])
   next()
@@ -19,18 +25,20 @@ var allowCrossDomain = (req, res, next) => {
 var app = express()
 app.use(bodyParser.json())
 app.use(allowCrossDomain)
-// app.use(auth)
 
 // Routes
-// app.use '/api/sessions', require './controllers/sessions'
-// app.use '/api/users', require './controllers/users'
-// app.use '/api/timeLogs', require './controllers/timeLogs'
+app.get('/yelp/sample', (req, res, next) => {
+  yelp.search({term: 'food', location: 'Jacksonville'})
+    .then((data) => {
+      return res.json(data)
+    })
+    .catch((err) => {
+      return next(err)
+    })
+})
 
 var server = http.createServer(app)
 app.set('server', server)
 app.get('server').listen(3000, () => {
   console.log('Server listening on :3000')
 })
-
-import Yelp = require('./yelp')
-// Yelp.sample()
