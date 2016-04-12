3fa91334a9a1ad53a36bc95cd5b6e9a74b957796
johnapost <notimetobeidle@gmail.com>
2016-04-12 01:22:33 -0400

    refactor Card and add most tests

---
+++ src/components/card-stack/card.spec.ts
@@ -0,0 +1,100 @@
+// TypeScript definitions
+/// <reference path='../../../typings/tsd.d.ts' />
+/// <reference path='../../../node_modules/angular2/typings/browser.d.ts'/>
+
+import {Card} from './card'
+import faker = require('faker')
+var card = Card.prototype
+
+describe('Card', () => {
+  it('constructor should set element to the native element', () => {
+    let nativeElement = faker.lorem.words(1)
+    let element = {nativeElement: nativeElement}
+
+    card.constructor(element)
+
+    expect(card.element).toEqual(nativeElement)
+  })
+
+  xdescribe('ngOnInit', () => {
+    it('should add hammer listener to the host element', () => {
+      card.ngOnInit()
+    })
+
+    it('should register hammer touch events', () => {
+
+    })
+
+    it('should call setPos if index is defined', () => {
+
+    })
+
+    it('should not call setPos if index is not defined', () => {
+
+    })
+  })
+
+  it("setPos should set the card's initial position in the card stack", () => {
+    let index = faker.random.number()
+    let calcBottom = `${10 * index + 16}px`
+    let calcScale = faker.random.number()
+    card.index = index.toString()
+    let element = {
+      nativeElement: {style: {zIndex: '', bottom: '', transform: ''}}
+    }
+    spyOn(card, 'calcScale').and.returnValue(calcScale)
+
+    card.constructor(element)
+    card.setPos()
+
+    expect(card.element.style.zIndex).toEqual(`-${index}`)
+    expect(card.element.style.bottom).toEqual(calcBottom)
+    expect(card.element.style.transform).toEqual(calcScale)
+  })
+
+  it('dragCard should set element style properties', () => {
+    let deltaX = faker.random.number()
+    let offsetLeft = faker.random.number()
+    let offset = deltaX - offsetLeft
+    let calcScale = faker.random.number()
+    let ev: any = {deltaX: deltaX}
+    let element = {
+      nativeElement: {
+        offsetLeft: offsetLeft, style: {transition: '', transform: ''}
+      }
+    }
+    spyOn(card, 'calcScale').and.returnValue(calcScale)
+
+    card.constructor(element)
+    card.dragCard(ev)
+
+    expect(card.element.style.transition).toEqual('none')
+    expect(card.element.style.transform).toEqual(`
+      translate3d(${offset}px, 0, 0)
+      ${calcScale}
+    `)
+  })
+
+  it('releaseCard should set element style properties', () => {
+    let calcScale = faker.random.number()
+    let element = {nativeElement: {style: {transition: '', transform: ''}}}
+    spyOn(card, 'calcScale').and.returnValue(calcScale)
+
+    card.constructor(element)
+    card.releaseCard()
+
+    expect(card.element.style.transition).toEqual('all 1s')
+    expect(card.element.style.transform).toEqual(`
+      translate3d(0px, 0, 0)
+      ${calcScale}
+    `)
+  })
+
+  it('calcScale should calculate the 3d scale based on index', () => {
+    let index = faker.random.number()
+    card.index = index.toString()
+    let val = 1 - index * .04
+
+    expect(card.calcScale()).toEqual(`scale(${val})`)
+  })
+})
